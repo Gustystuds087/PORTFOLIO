0e0b4d3c21fac6467856c5a243499c32656839a7
Anshul <sanshul087@gmail.com>
2023-12-10 17:37:40 +0530

    changes1

---
+++ src/assets/images/index.js
@@ -0,0 +1,13 @@
+import hero from './hero.jpg'
+import logo from './logo.svg'
+import logo1 from './logo1.png'
+import vs from './vs.png'
+import LOGOO from './LOGOO.png'
+
+export {
+    hero,
+    logo,
+    logo1, 
+    vs,
+    LOGOO
+}
---
+++ src/constants/index.js
@@ -0,0 +1,203 @@
+import { logo1, vs, LOGOO } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "React.js Developer",
+        company_name: "Crowd Control Esports",
+        icon: logo1,
+        iconBg: "#accbe1",
+        date: "December 2022 - Februrary 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "React.js Developer",
+        company_name: "CodeClause",
+        icon: vs,
+        iconBg: "#fbc3bc",
+        date: "Jan 2021 - Feb 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "Khelshala",
+        icon: LOGOO,
+        iconBg: "#b7e4c7",
+        date: "Jan 2023 - May 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Gustystuds087#hi--im-anshul',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/sanshul087/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'KitPlace',
+        description: 'Designed a website with a comprehensive database of hiring companies, including placement records and relevant details.',
+        link: 'https://github.com/Gustystuds087/KITPLACE',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'secureus',
+        description: 'Created an user dashboard to save their warranty cards. Used Cron to schedule the program to execute automatically as the warranty period is about to expire.',
+        link: 'https://github.com/Gustystuds087/secureus',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: '3D Apple Website Clone ',
+        description: 'Created a 3D website clone of the official Apple website. Used Three.js to import the 3D model and worked upon its dynamics and mechanics to make it mobile.',
+        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Tshirt_Customizer',
+        description: 'Built a website using a 3d model of a tshirt enabling users to customize it accoridingly by changing colors,  patterns and uploading logos from the local storage of the users.',
+        link: 'https://github.com/Gustystuds087/Tshirt_customizer',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Coming soon',
+        description: '...',
+        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+    }
+];
