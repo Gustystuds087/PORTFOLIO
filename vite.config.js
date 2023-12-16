ab4989f09948330385ff9028906d7bbf0744c332
Anshul <sanshul087@gmail.com>
2023-12-16 16:27:18 +0530

    corrected vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-})
+  build: {
+    // You can add other build configurations here
+
+    // Code splitting configuration
+    chunkSizeWarningLimit: 1200, // Adjust this value as needed
+    output: {
+      manualChunks: {
+        lodash: ['lodash']
+      },
+    },
+  },
+});
